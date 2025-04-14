7e0ff8219c1c7d45030ecba8ea349df63c9eb2ea
CoderJo-Pro <coderjo@outlook.com>
2025-04-14 11:27:04 +0800

    update(ui): redesign the default layout

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,15 +1,13 @@
 <template>
-  <div class="w-full h-screen p-2 flex flex-row bg-base-200 gap-2">
-    <div class="box-content w-42 flex flex-col">
+  <div class="w-full h-screen flex bg-base-100">
+    <div class="w-1/3 p-3 flex flex-col gap-4 items-center">
       <div
-        class="w-full p-4 aspect-square flex flex-col justify-center font-mono font-bold text-3xl select-none"
+        class="w-34 px-4 py-1 flex flex-col justify-center font-mono font-bold text-2xl select-none"
       >
         <div class="self-start">Bible</div>
         <div class="self-end">Viewer</div>
       </div>
-      <ul
-        class="box-border w-full h-full border border-current/20 rounded-box bg-base-100 menu font-bold text-current/80 tracking-wider"
-      >
+      <ul class="w-full h-full p-0 menu font-bold text-current/80 tracking-wider">
         <li v-for="link in links">
           <NuxtLink :to="link[1]" :class="{ 'menu-active': isActive(link[1]) }">
             {{ link[0] }}
@@ -17,7 +15,7 @@
         </li>
       </ul>
     </div>
-    <main class="w-full border border-current/20 overflow-hidden rounded-box">
+    <main class="w-full overflow-hidden">
       <div ref="main" class="h-full overflow-y-auto bg-base-100">
         <slot></slot>
       </div>
